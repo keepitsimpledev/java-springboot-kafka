48275f905672dd2a08196bad216f6967cb060b51
dev <keep.it.simple.dev@gmail.com>
2026-09-22 15:45:28 -0400

    fix spacing

--- .github/workflows/container-check.sh
+++ .github/workflows/container-check.sh
@@ -18,14 +18,14 @@ if [ $CURL_DKCON_SUCCESS = false ]; then
     echo "waited a total of $((i*3)) seconds - checking"
     CURL_DKCON=$(curl --silent --fail localhost:8887) || true # `|| true` will prevent failure,
     CURL_DKCON_LENGTH=${#CURL_DKCON}
-  if [ $CURL_DKCON_LENGTH -gt 0 ]; then
-    CURL_DKCON_SUCCESS=true
-    echo "container dkcon successfully initalized"
-    curl localhost:8887
-    break
-  elif [ $i -eq 20 ]; then
-    echo "container dkcon failed to initalize"
-    curl localhost:8887
-  fi
+    if [ $CURL_DKCON_LENGTH -gt 0 ]; then
+      CURL_DKCON_SUCCESS=true
+      echo "container dkcon successfully initalized"
+      curl localhost:8887
+      break
+    elif [ $i -eq 20 ]; then
+      echo "container dkcon failed to initalize"
+      curl localhost:8887
+    fi
   done
 fi
